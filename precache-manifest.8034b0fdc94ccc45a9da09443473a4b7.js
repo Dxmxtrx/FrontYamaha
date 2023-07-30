a29906053320a5ca8b9f3d35536561ec69885e8f
Dxmxtrx <demetriodantasferreira@gmail.com>
2023-07-30 20:44:11 -0300

    Updates

---
+++ precache-manifest.8034b0fdc94ccc45a9da09443473a4b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "035c8ddb6e1113104201",
+    "url": "/FrontYamaha-github.io/static/css/main.b5685c90.chunk.css"
+  },
+  {
+    "revision": "035c8ddb6e1113104201",
+    "url": "/FrontYamaha-github.io/static/js/main.035c8ddb.chunk.js"
+  },
+  {
+    "revision": "a707d2677c8f4f7b8d66",
+    "url": "/FrontYamaha-github.io/static/js/1.a707d267.chunk.js"
+  },
+  {
+    "revision": "509f77d64df521b69151",
+    "url": "/FrontYamaha-github.io/static/js/runtime~main.509f77d6.js"
+  },
+  {
+    "revision": "1650132350f15261cb195459fd1b0136",
+    "url": "/FrontYamaha-github.io/static/media/Road_Rage.16501323.otf"
+  },
+  {
+    "revision": "45066bafd9269cb51f9c4f8ca2c34832",
+    "url": "/FrontYamaha-github.io/static/media/DESIGNER.45066baf.otf"
+  },
+  {
+    "revision": "18f1f200850c8b344d2dd4872b9e1d9b",
+    "url": "/FrontYamaha-github.io/static/media/Logo.18f1f200.png"
+  },
+  {
+    "revision": "f534d42e955fdea024a00e8e4da021d2",
+    "url": "/FrontYamaha-github.io/static/media/Logo2.f534d42e.png"
+  },
+  {
+    "revision": "953da49a1bd2d4491123f87ecb6a69f1",
+    "url": "/FrontYamaha-github.io/static/media/MT.953da49a.png"
+  },
+  {
+    "revision": "ef03b12b6777dd8a95fe1beb8a1d5909",
+    "url": "/FrontYamaha-github.io/static/media/Image.ef03b12b.png"
+  },
+  {
+    "revision": "353ee5ff525655a71896bbf0beb06018",
+    "url": "/FrontYamaha-github.io/index.html"
+  }
+];
